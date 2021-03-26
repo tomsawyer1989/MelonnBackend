ee23d7bdcfe6cfa2ae84d232b28e068929abbfa5
Jorge Leonardo Herrera Ortiz <herrera.jorge@correounivalle.edu.co>
2021-03-25 22:24:31 -0500

    feat(orderRouter): get post and delete implemented

--- routers/orderRouter.js
+++ routers/orderRouter.js
@@ -2,18 +2,44 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const orderRouter = express.Router();
-
 orderRouter.use(bodyParser.json());
 
+const orderList = [];
+let id = 0;
+
 orderRouter.route('/')
-.get(async (req,res) => {
-    // try {
-    //     const {rows} = await pool.query('SELECT * FROM order');
-    //     res.send(rows);
-    // } 
-    // catch (error) {
-    //     console.log(error);
-    // }
+.get(async (req, res) => {
+    try {
+        res.send(orderList);
+    } 
+    catch (error) {
+        console.log(error);
+    }
+})
+.post(async (req, res) => {
+    try {
+        id = id + 1;
+        const order = req.body;
+        order.id = id;
+        orderList.push(order);
+        res.send(order);
+    } 
+    catch (error) {
+        console.log(error);
+    }
+});
+
+orderRouter.route('/:id')
+.delete(async (req, res) => {
+    try {
+        let id = parseInt(req.params.id);
+        let index = orderList.map(item => {return item.id;}).indexOf(id);
+        index !== -1 && orderList.splice(index, 1);
+        res.send('order deleted');
+    } 
+    catch (error) {
+        console.log(error);
+    }
 });
 
 module.exports = orderRouter;
